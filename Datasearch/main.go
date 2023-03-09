fceb31c89d667267c7147d61da88f1501aa83329
victorbischoff <morkebergv@gmail.com>
2023-03-09 16:22:45 +0100

    scond commit

--- Datasearch/main.go
+++ Datasearch/main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"datasearch/pkg/binary"
-)
-
 func main() {
-	listOfWords := []string{"apple", "banana", "kiwi", "orange", "pear"}
-	findWord := "banana"
-	binary.BinarySearchWithSort(listOfWords, findWord)
+	
 }
---
+++ Newton/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func f(x float64) float64 {
+	return math.Pow(x, 3) - 5*math.Pow(x, 2) + 3*x + 7
+}
+
+func fPrime(x float64) float64 {
+	return 3*math.Pow(x, 2) - 10*x + 3
+}
+
+func newtonRaphson(x0 float64, tolerance float64) float64 {
+	x := x0
+	deltaX := f(x) / fPrime(x)
+	for math.Abs(deltaX) >= tolerance {
+		x -= deltaX
+		deltaX = f(x) / fPrime(x)
+	}
+	return x
+}
+
+func main() {
+	root := newtonRaphson(2, 0.0001)
+	fmt.Printf("Rod af funktionen: %f\n", root)
+}
